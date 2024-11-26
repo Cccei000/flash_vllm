8b0a8f8c625a10b97dce8480a10dcaaf6ab517c3
cccei000 <lincong_ceiling@126.com>
2024-11-26 16:26:05 +0800

    add nov26 exp

--- launch_latency_auto.sh
+++ launch_latency_auto.sh
@@ -2,12 +2,12 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WORLD_SIZE=4
 export TOKENIZERS_PARALLELISM=true
+export VLLM_ATTENTION_BACKEND="FLASHINFER"
 
-output_lens=(1)
-input_lens=(1024)
-batch_size=(2)
-model="Llama-2-7b-chat-hf"
-use_flashinfer=(1)
+input_lens=(4096)
+batch_size=(1)
+models=("Meta-Llama-3.1-8B-Instruct")
+use_fp16=(1 0)
 
 
 if [ $# -eq 0 ]; then
@@ -21,33 +21,39 @@ mkdir -p "$save_path"
 
 
 for input_len in "${input_lens[@]}"; do
-    for output_len in "${output_lens[@]}"; do
-        for bs in "${batch_size[@]}"; do
-            for flashinfer_flag in "${use_flashinfer[@]}"; do
+    for bs in "${batch_size[@]}"; do
+        for model in "${models[@]}"; do
+            for fp16_flag in "${use_fp16[@]}"; do
 
-                if [ $flashinfer_flag -eq 1 ]; then
-                    export VLLM_ATTENTION_BACKEND="FLASHINFER"
-                    tag="w_flashinfer"
+                if [ $fp16_flag -eq 1 ]; then
+                    export VLLM_USE_FLASHINFER_FP16_ACCUM=1
+                    tag="w_flashinfer_fp16"
                 else
-                    tag="wo_flashinfer"
+                    tag="wo_flashinfer_fp16"
                 fi
 
                 model_path="/models/${model}"
-                save_name="${model}_inlen${input_len}_outlen${output_len}_bs${bs}_${tag}"
+                save_name="${model}_inlen${input_len}_outlen1_bs${bs}_${tag}"
                 echo -e "\n\n\n######## Running Exp ${save_name} ########\n\n\n"
                 
-                command="ncu -k regex:\\(\\?i\\).*prefill.* --set full --replay-mode kernel \
-                    -f -o ${save_name}_reduction --devices 0 \
-                    python ./benchmark_latency.py \
-                    --input-len ${input_len} --output-len ${output_len} --batch-size ${bs} \
+                # command="ncu -k regex:\\(\\?i\\).*prefill.* --set full --replay-mode kernel \
+                #     -f -o ${save_name} --devices 0 \
+                #     python ./benchmark_latency.py \
+                #     --input-len ${input_len} --output-len 1 --batch-size ${bs} \
+                #     --num-iters-warmup 0 --num-iters 1 \
+                #     --model ${model_path} -tp 4 --max-model-len 4096 \
+                #     --trust-remote-code --dtype float16 \
+                #     --output-json ${save_path}/${save_name}.json"  
+
+                command="python ./benchmark_latency.py \
+                    --input-len ${input_len} --output-len 1 --batch-size ${bs} \
                     --num-iters-warmup 0 --num-iters 1 \
-                    --model ${model_path} -tp 4 --max-model-len 4096 \
+                    --model ${model_path} -tp 4 --max-model-len 8192 \
                     --trust-remote-code --dtype float16 \
-                    --output-json ${save_path}/${save_name}.json"
-    
+                    --output-json ${save_path}/${save_name}.json"    
                 eval $command
 
-                unset VLLM_ATTENTION_BACKEND
+                unset VLLM_USE_FLASHINFER_FP16_ACCUM
                 sleep 5
             done
         done
