d618567e6206afbe854d3469b61016812fd972f4
cccei000 <lincong_ceiling@126.com>
2024-11-26 18:07:46 +0800

    add throughput exp

--- launch_throughput_auto.sh
+++ launch_throughput_auto.sh
@@ -2,10 +2,11 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WORLD_SIZE=4
 export TOKENIZERS_PARALLELISM=true
+export VLLM_ATTENTION_BACKEND="FLASHINFER"
 
-output_lens=(1024)
+output_lens=(128 1024)
 models=("Meta-Llama-3.1-8B-Instruct")
-use_flashinfer=(1)
+use_fp16=(1 0)
 
 
 if [ $# -eq 0 ]; then
@@ -20,13 +21,13 @@ mkdir -p "$save_path"
 
 for output_len in "${output_lens[@]}"; do
     for model in "${models[@]}"; do
-        for flashinfer_flag in "${use_flashinfer[@]}"; do
+        for fp16_flag in "${use_fp16[@]}"; do
 
-            if [ $flashinfer_flag -eq 1 ]; then
-                export VLLM_ATTENTION_BACKEND="FLASHINFER"
-                tag="w_flashinfer"
+            if [ $fp16_flag -eq 1 ]; then
+                export VLLM_USE_FLASHINFER_FP16_ACCUM=1
+                tag="w_flashinfer_fp16"
             else
-                tag="wo_flashinfer"
+                tag="wo_flashinfer_fp16"
             fi
 
             model_path="/models/${model}"
@@ -36,7 +37,7 @@ for output_len in "${output_lens[@]}"; do
             command="python ./benchmark_throughput.py \
                 --backend vllm \
                 --dataset ./ShareGPT_V3_unfiltered_cleaned_split.json \
-                --model ${model_path} \
+                --model ${model_path} --dtype float16 \
                 --tokenizer ${model_path} \
                 --output-json ${save_path}/${save_name}.json \
                 --trust-remote-code \
@@ -45,7 +46,7 @@ for output_len in "${output_lens[@]}"; do
 
             eval $command
 
-            unset VLLM_ATTENTION_BACKEND
+            unset VLLM_USE_FLASHINFER_FP16_ACCUM
             sleep 5
         done
     done
